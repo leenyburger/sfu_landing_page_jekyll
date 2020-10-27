059bb8e173770f0690b2086ed9d4f996d55563ce
Mike Rogers <me@mikerogers.io>
2020-10-27 17:47:28 +0000

    Pushing CSS compression off to netlify

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer'),
-    require('cssnano')({ preset: 'default' }),
-    ...(process.env.JEKYLL_ENV == "production" ? [require("cssnano")({ preset: "default" })] : [])
+    require('autoprefixer')
   ]
 }
