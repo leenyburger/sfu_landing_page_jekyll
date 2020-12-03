8671ca1c685ce6b272299d29ca476a57a03bc3e1
Colleen Schnettler <leenyburger@users.noreply.github.com>
2020-12-03 13:09:57 -0500

    Update image spacing to not squish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: false,
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '1': '100%'
+      }
+    },
   },
   variants: {},
   plugins: [],
