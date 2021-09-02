c324cf7f342d0748c7445fe3137da2552cb06eba
Colleen Schnettler <leenyburger@users.noreply.github.com>
2021-09-02 13:26:32 -0700

    In progress add clipboard functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // SFU emits fileUploadSuccess every time a file has completed loading
   el.addEventListener("fileUploadSuccess", function (e) {
-    const snippet = document.getElementById("snippet")
-    snippet.show
-    alert("Your file uploaded successfully and can be see at this URL: " + this.value)
+    const snippet = document.getElementById("snippet");
+    snippet.classList.remove('hide');
+   console.log ("Your file uploaded successfully and can be see at this URL: " + this.value)
   });
 });
