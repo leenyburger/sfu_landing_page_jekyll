dc2ea540181df20bd43efad22d1bdce6827b94f7
Colleen Schnettler <leenyburger@users.noreply.github.com>
2021-09-02 17:24:53 -0700

    Use simple file upload on homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // SFU emits fileUploadSuccess every time a file has completed loading
   el.addEventListener("fileUploadSuccess", function (e) {
+    // The snippet that shows up on file success to allow user to copy URL
     const snippet = document.getElementById("snippet");
-    snippet.classList.remove('hide');
-   console.log ("Your file uploaded successfully and can be see at this URL: " + this.value)
+    const copyButton = document.getElementById("file-url");
+    copyButton.setAttribute('data-clipboard-text', this.value)
+    snippet.classList.remove('hidden');
   });
 });
+/////////////////////////////////////////////////////////////////////////////////
+// Popper.js for tooltips
+  function togglePopover(event, tooltipID) {
+    let element = event.target;
+    while (element.nodeName !== "BUTTON") {
+      element = element.parentNode;
+    }
+    let tooltipElement = document.getElementById(tooltipID);
+    var popper = Popper.createPopper(element, tooltipElement, {
+      placement: 'bottom'
+    });
+    // Show the tooltip
+    tooltipElement.classList.remove("hidden");
+    // Close the tooltip
+    setTimeout(function() {
+      tooltipElement.classList.add("hidden")
+    }, 2000);
+  }
+
+//////////////////////////////////////////////////////////////////////////////////
+// ClipboardJS
+document.addEventListener('DOMContentLoaded', () => {
+
+  const clipboard = new ClipboardJS('#file-url');
+
+  clipboard.on('success', function(e) {
+    togglePopover(event,'tooltip-copied')
+  });
+
+  clipboard.on('error', function(e) {
+    const copyTooltip = document.getElementById("copy-tooltip")
+    copyTooltip.innerHTML = "Failed to Copy"
+    togglePopover(event,'tooltip-copied')
+  });
+});
+
+
